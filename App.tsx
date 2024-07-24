c9d39c8c4cced075d69f7635d8b736243a763b26
Umar <umar.dev500@gmail.com>
2024-07-24 20:58:08 +0700

    change prettier config

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
+import type { PropsWithChildren } from 'react';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -29,7 +29,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
